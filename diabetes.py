0104c158e12702e4b202342a0a313f3e51a7420b
Nguyen Nam <60035507+tavenguyen@users.noreply.github.com>
2025-11-30 11:04:11 +0700

    (add) model validation.

--- diabetes.py
+++ diabetes.py
@@ -7,6 +7,9 @@ from typing import Any
 import pandas as pd
 import numpy as np
 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
 # Medical Standards Threshold (Dựa trên Y khoa)
 GLUCOSE_THRESHOLD = 140 # (>= 140 : 1 | < 140: 0) 
 BMI_THRESHOLD = 30
@@ -50,6 +53,30 @@ def process(evidence : dict):
 df = pd.read_csv("diabetes_dataset.csv")
 total_samples = len(df)
 
+cols_missing = ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']
+means = dict()
+for key in cols_missing:
+    for value in df[key]:
+        means[key] = means.get(key, 0.0) + value
+    means[key] = means[key] / total_samples
+
+for key in cols_missing:
+    df[key] = df[key].replace(0, means[key])
+
+y = df['Outcome']
+
+# Tham số quan trọng: stratify=y
+X_train, X_test = train_test_split(df, test_size=0.2, random_state=42, stratify=y)
+
+print(f"Kích thước Train: {len(X_train)}")
+print(f"Kích thước Test:  {len(X_test)}")
+
+# Kiểm tra xem tỷ lệ bệnh nhân có được bảo toàn không?
+print("\nTỷ lệ người bệnh (Outcome=1):")
+print(f"- Gốc:   {df['Outcome'].mean():.2%}")
+print(f"- Train: {X_train['Outcome'].mean():.2%}")
+print(f"- Test:  {X_test['Outcome'].mean():.2%}")
+
 # Nodes
 df['Glucose'] = (df['Glucose'] >= GLUCOSE_THRESHOLD).astype(int)
 df['BMI'] = (df['BMI'] >= BMI_THRESHOLD).astype(int)
@@ -60,6 +87,15 @@ df['Insulin'] = (df['Insulin'] >= INSULIN_THRESHOLD).astype(int)
 df['SkinThickness'] = (df['SkinThickness'] >= SKIN_THICKNESS_THRESHOLD).astype(int)
 df['DiabetesPedigreeFunction'] = (df['DiabetesPedigreeFunction'] >= PEDIGREE_FUNCTION_THRESHOLD).astype(int)
 
+y = df['Outcome']
+
+# Tham số quan trọng: stratify=y
+X_train, X_test = train_test_split(df, test_size=0.2, random_state=42, stratify=y)
+
+print("\n(Outcome=1):")
+print(f"Train: {X_train['Outcome'].mean():.2%}")
+print(f"Test:  {X_test['Outcome'].mean():.2%}")
+
 # Diabetes (Outcome)
 model = BayesianNetwork([
     ('Age', 'Pregnancies'), ('Age', 'Glucose'), ('Age', 'BloodPressure'), ('Age', 'Outcome'), 
@@ -69,18 +105,19 @@ model = BayesianNetwork([
     ('Pregnancies', 'Outcome')
 ])
 
-model.fit(df, estimator=MaximumLikelihoodEstimator)
+model.fit(X_train, estimator=MaximumLikelihoodEstimator)
 model.check_model()
 
-# Result
-infer = VariableElimination(model)
+test_data_no_label = X_test.drop(columns=['Outcome'])
+y_pred_df = model.predict(test_data_no_label)
+y_pred = y_pred_df['Outcome']
+y_true = X_test['Outcome']
 
-# P(Outcome | Age = 50, Glucose = 130, BMI = 29)
-dist_evidence = {
-    'Age': 50,
-    'Glucose': 130,
-    'BMI': 29
-}
-dist_evidence = process(dist_evidence)
-w_dist = infer.query(['Outcome'], dist_evidence)
-print(w_dist)
+acc = accuracy_score(y_true, y_pred)
+conf_matrix = confusion_matrix(y_true, y_pred)
+report = classification_report(y_true, y_pred)
+
+print(f"(Accuracy): {acc:.4f}")
+print("\n(Confusion Matrix):")
+print(conf_matrix)
+print(report)
